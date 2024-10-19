cbf08ef2ab20e27a71191d21a3a54599baccee7a
Anubhab2003 <anubhabchowdhury01@gmail.com>
2024-10-20 00:21:28 +0530

    basics

---
+++ Basic/varirable.js
@@ -0,0 +1,12 @@
+const accountId=1554
+let accountEmail="anubhabchowdhury01@gmail.com"
+var accountPassword="password"
+accountCity="Silliguri"
+
+// accountId=45//Not allowed
+accountEmail="Partha.pratim.goswami@gds.ey.com"
+accountPassword="newpassword"
+accountCity="Kolkata"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+//PLEASE DON'T USE VAR beacuse of issue in block scope
