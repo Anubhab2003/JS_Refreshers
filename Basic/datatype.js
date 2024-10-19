f90dfc8e309ebe91c1f19d78cce11dd909ba7119
Anubhab2003 <anubhabchowdhury01@gmail.com>
2024-10-20 00:39:57 +0530

    datatypes

---
+++ Basic/datatype.js
@@ -0,0 +1,23 @@
+"use strict"; //Treate all code as new version of JS
+// alert(3+3) //We are using Node js not browser so alert will not work
+
+let name="Anubhab"
+let age=21
+let isLoggedIn=true
+
+
+//Number => 2 to power 53
+
+//bigint
+
+//string=>""
+
+//boolean
+//null =>Stand alone value=> Empty value Null is also a Object
+//undefined=> not given a value to a variable
+//symbol=>unique value
+
+//object=>store multiple values JSON=>Javascript Object Notation
+console.log(typeof undefined)
+
+
