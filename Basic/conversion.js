ec830428d242d97529af065f1d7db2f0b15e61dd
Anubhab2003 <anubhabchowdhury01@gmail.com>
2024-10-20 01:00:19 +0530

    Conversions

--- Basic/conversion.js
+++ Basic/conversion.js
@@ -1,2 +1,19 @@
-let score=100
-console.log(typeof score)
+// let score=true
+// console.log(typeof score)
+
+// let valueInNumber=Number(score)
+// console.table([valueInNumber,typeof valueInNumber])
+
+// // "33"=> 33
+// //"33anubhab"=>NaN
+// //"true"=>1
+// //"false"=>0
+// let isLoggedIn=1
+
+
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.table([booleanIsLoggedIn,typeof booleanIsLoggedIn])
+
+let someNumber=56
+let stringNumber=String(someNumber)
+console.table([stringNumber,typeof stringNumber])
--- Basic/output.md
+++ Basic/output.md
@@ -1,1 +1,5 @@
 ![alt text](image.png)
+
+REMEMBER CONVERSION IN JS IS A BIT HEADACHE 
+nan is not a number
+![alt text](image-1.png)
