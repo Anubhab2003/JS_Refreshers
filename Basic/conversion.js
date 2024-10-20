33ff3c76763f3a9417e4b1e04332941550f05364
Anubhab2003 <anubhabchowdhury01@gmail.com>
2024-10-20 20:03:08 +0530

    conversion

--- Basic/conversion.js
+++ Basic/conversion.js
@@ -17,3 +17,23 @@
 let someNumber=56
 let stringNumber=String(someNumber)
 console.table([stringNumber,typeof stringNumber])
+
+//OPERATIONS
+let value=4
+
+let negValue=-value
+console.log(negValue)
+
+let str1="Anubhab"
+let str2=" Peter"
+let str3=str1+str2
+console.log(str3)
+console.log("1"+2)
+console.log("1"+2+2)
+console.log(1+2+"2")
+console.log("1"+(2+2))
+
+// Difference between == and === is that == does type conversion and === does not do type conversion
+console.log(1=="1")
+console.log(1==="1")
+//== checks only value === checks both value and type
