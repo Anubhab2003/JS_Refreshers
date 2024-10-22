85b11934650516df197ab758f3484f4be5126c10
Anubhab2003 <anubhabchowdhury01@gmail.com>
2024-10-22 23:24:12 +0530

    Strings

---
+++ Basic/string.js
@@ -0,0 +1,13 @@
+const name="Anubhab"
+const repoCount=50
+const gameName= new String("Cricket-game")
+
+console.log(`Name is ${name} and repoCount is ${repoCount}`);
+console.log(`Game Name is ${gameName.toUpperCase()}`); // Game Name is CRICKET
+console.log(gameName.charAt(6))
+const newStr=gameName.substring(0,6)
+console.log(newStr)
+const spiderMan="         -Peter-Parker -        "
+console.log(spiderMan)
+console.log(spiderMan.trim())
+// console.log(spiderman.split('-'));//Convert in a Array
