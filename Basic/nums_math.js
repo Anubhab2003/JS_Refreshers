30ecebf12285eff943d818ba657b0030d818414e
Anubhab2003 <anubhabchowdhury01@gmail.com>
2024-10-28 13:28:08 +0530

    Math

---
+++ Basic/nums_math.js
@@ -0,0 +1,28 @@
+// const score=500;
+// const balance=new Number(100);
+// console.log(balance.toString().length); // 100
+// console.log(score+balance); // 600
+// console.log(balance.toFixed(2)); // 100.00
+
+// const otherNumber=23.155555
+// console.log(otherNumber.toPrecision(3)); // 23.16
+
+// const hundreds=100000000
+// console.log(hundreds.toLocaleString('en-IN')); // 100,000,000
+
+// const smallNumber=0.000000000000001
+// console.log(smallNumber.toLocaleString('en-IN')); // 0
+// //+++++++++++++++++++++++++MATHS+++++++++++++++++++++++++++++++++++
+
+
+// console.log(Math.abs(-45545458425));
+// console.log(Math.round(4.2));
+
+
+
+// console.log(Math.random())
+// console.log((Math.random()*10)+1)
+
+const min=1
+const max=6
+console.log(Math.floor(Math.random()*(max-min+1))+min)cd .
